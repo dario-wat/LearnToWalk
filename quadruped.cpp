8a0e654afaa77b3e032df4a70d85bd4287062c48
dario-wat <dario.vidas92@gmail.com>
2015-05-23 17:03:54 +0200

    Cleanup

--- quadruped.cpp
+++ quadruped.cpp
@@ -17,6 +17,8 @@
 #include "visualizator.hpp"
 
 
+//TODO CFM and ERP parameters??
+
 // ga.cpp
 // TODO crossing maybe average
 
@@ -25,27 +27,26 @@
 
 //evaluator
 // TODO what fitness??
-// TODO send erp and cfn params thrpough the constructor instead of using constants
+// TODO send erp and cfm params thrpough the constructor instead of using constants
 
 // TODO in robot.cpp random initialization is maybe not so random
 // TODO in robot.cpp walk function, there was some weird walking code
 
 using std::cout;
 using std::cin;
+using std::cerr;
 using std::endl;
-
-
-static const double para_K = 100000.0;	//elastic modulus
-static const double para_C = 1000.0;	//viscous modulus
 
 
 // Constants for world initialization
 static const double GRAVITY = -9.81;
 static const double CFM_P = 10e-10;
 static const double ERP_P = 0.2;
-
+static const double para_K = 100000.0;	//elastic modulus
+static const double para_C = 1000.0;	//viscous modulus
 
 static const double SIM_TIME = 20.0;
+static const double SIM_STEP = 0.005;
 static const int GENERATIONS = 300;
 static const int POP_SIZE = 100;
 
@@ -54,11 +55,11 @@ static const int mid_l = 10;
 static const int out_l = LEG_NUM*(JT_NUM+1);
 static const int c_size = (in_l+1)*mid_l + (mid_l+1)*out_l;
 
-
+// Auxiliary arrays
 static double f_layer[mid_l * (in_l+1)];
 static double s_layer[out_l * (mid_l+1)];
 
-
+// For simulation
 static Evaluator* evaluator;
 static Robot* rob;
 static ANN* ann;
@@ -66,29 +67,11 @@ static GA* ga;
 static std::vector<double*> population(POP_SIZE);
 static std::vector<double> fitness(POP_SIZE);
 
-
 dWorldID world;
 dSpaceID space;
 
 
-static void encode(double (& fl)[mid_l * (in_l+1)],
-	double (& sl)[out_l * (mid_l+1)],
-	double* chromosome
-	) {
-
-	int f_lim = mid_l * (in_l+1);
-	int s_lim = out_l * (mid_l+1);
-	int k = 0;
-	for (int i = 0; i < f_lim; i++) {
-		chromosome[k] = fl[i];
-		k++;
-	}
-	for (int i = 0; i < s_lim; i++) {
-		chromosome[k] = sl[i];
-		k++;
-	}
-}
-
+// Decodes chromosome into 2 weight matrices
 static void decode(double (& fl)[mid_l * (in_l+1)],
 	double (& sl)[out_l * (mid_l+1)],
 	double* chromosome
@@ -108,81 +91,71 @@ static void decode(double (& fl)[mid_l * (in_l+1)],
 }
 
 
-void simulate() {
+// Runs learning process
+static void simulate() {
 	for (int j = 0; j < GENERATIONS; j++) {
 		for (int i = 0; i < POP_SIZE; i++) {
-			rob = new Robot(world, space, 0.005);
+			rob = new Robot(world, space, SIM_STEP);
 			decode(f_layer, s_layer, population[i]);
 			ann->setWeights(f_layer, s_layer);
 			fitness[i] = evaluator->evaluate(rob, ann);
 			delete rob;
-			cout << "Ind: " << i << " Fitness: " << fitness[i] << endl;
+			cerr << "Ind: " << i << " Fitness: " << fitness[i] << endl;
 		}
-		cout << endl;
+		cerr << endl;
+		
 		ga->evolve(fitness, population);
+		
 		GA::printChromosome(population[0], c_size);
-		cout << j<< endl;
+		cout << j << endl;
 	}
 }
 
-
-void command(int cmd) {
-	if (cmd == 'q') {
-		exit(0);
-	}
-}
-
-void initializeCallbacks(dsFunctions* fn) {
-	fn->version = DS_VERSION;
-	fn->start = 0;//&start;
-	fn->step = 0;//&simLoop;
-	fn->command = &command;
-	fn->stop = 0;
-	fn->path_to_textures = "textures";
-}
 
 int main(int argc, char** argv) {
 	srand(time(NULL));
 	
 	// Initialize things for ODE
-	dInitODE2();
-	world = dWorldCreate();        			// for dynamics
-	space = dHashSpaceCreate(0);        		// for collision
+	dInitODE2(0);
+	world = dWorldCreate();        						// for dynamics
+	space = dHashSpaceCreate(0);        				// for collision
 	dGeomID ground = dCreatePlane(space, 0, 0, 1, 0);	// ground
-	dsFunctions fn;           							// function of drawstuff
-	initializeCallbacks(&fn);
-	evaluator = new Evaluator(world, space, 0.005, SIM_TIME);
-	
-	// TODO he set gravitiy as 0 in the beginning
 	dWorldSetGravity(world, 0, 0, GRAVITY);
+	dWorldSetCFM(world, 1.0/(SIM_STEP*para_K+para_C));
+	dWorldSetERP(world, (SIM_STEP*para_K)/(SIM_STEP*para_K + para_C));
 	
-	// TODO he did something else here
-	dWorldSetCFM(world, 1.0/(0.005*para_K+para_C));
-	dWorldSetERP(world, (0.005*para_K)/(0.005*para_K + para_C));
-	
-	ann = new ANN(in_l, mid_l, out_l);
-
+#ifndef DRAWIT
+	// Random population
 	for (int i = 0; i < POP_SIZE; i++) {
 		population[i] = new double[c_size];
 		GA::randomizeChromosome(population[i], c_size);
 	}
 	ga = new GA(c_size, POP_SIZE, 0.1, 15);
+	ann = new ANN(in_l, mid_l, out_l);
+	evaluator = new Evaluator(world, space, SIM_STEP, SIM_TIME);
 
-	// Run endless loop
-	//dsSimulationLoop(argc, argv, WINDOW_WIDTH, WINDOW_HEIGHT, &fn);
-#ifndef DRAWIT
-	simulate();
+	simulate();			// Learn
+
+	for (int i = 0; i < POP_SIZE; i++) {
+		delete population[i];
+	}
+	delete ga;
+	delete ann;
+	delete evaluator;
 #endif
+
 #ifdef DRAWIT
 	double chr[c_size];
 	for (int i = 0; i < c_size; i++) {
 		scanf("%la", chr+i);
-		cout << chr[i] << " ";
 	}
-	rob = new Robot(world, space, 0.005);
 	decode(f_layer, s_layer, chr);
+	ann = new ANN(in_l, mid_l, out_l);
 	ann->setWeights(f_layer, s_layer);
-	Visualizator::simulationLoop(argc, argv, rob, ann, world, space, 0.005);
+	rob = new Robot(world, space, SIM_STEP);
+	Visualizator::simulationLoop(argc, argv, rob, ann, world, space, SIM_STEP);
+	delete rob;
+	delete ann;
 #endif
 	
 	// Finalization and exiting
